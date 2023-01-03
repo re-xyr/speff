8f46f8c802672429baf6666f97a5c71aa80de1ba
Xy Ren <xy.r@outlook.com>
2023-01-03 10:29:37 +0800

    Add some haddock comments

--- src/Sp/Util.hs
+++ src/Sp/Util.hs
@@ -36,13 +36,16 @@ import           Data.Kind           (Type)
 import           Data.Tuple          (swap)
 import           Sp.Internal.Monad
 
+-- | Provides an environment value of type @r@, and you can override it in a local scope.
 data Reader (r :: Type) :: Effect where
   Ask :: Reader r m r
   Local :: (r -> r) -> m a -> Reader r m a
 
+-- | Obtain the environment value.
 ask :: Reader r :> es => Eff es r
 ask = send Ask
 
+-- | Override the environment value in a local scope.
 local :: Reader r :> es => (r -> r) -> Eff es a -> Eff es a
 local f m = send (Local f m)
 
@@ -51,20 +54,26 @@ handleReader !r = \case
   Ask       -> pure r
   Local f m -> interpose (handleReader (f r)) m
 
+-- | Run the 'Reader' effect with an environment value.
 runReader :: r -> Eff (Reader r : es) a -> Eff es a
 runReader r = interpret (handleReader r)
 
-data State s m a where
+-- | Provides a mutable state of type @s@.
+data State s :: Effect where
   Get :: State s m s
   Put :: s -> State s m ()
   State :: (s -> (a, s)) -> State s m a
 
+-- | Get the mutable state.
 get :: State s :> es => Eff es s
 get = send Get
 
+-- | Write a new value to the mutable state.
 put :: State s :> es => s -> Eff es ()
 put x = send (Put x)
 
+-- | Apply a function of type @s -> (a, s)@ on the mutable state, using the returned @s@ as the new state and
+-- returning the @a@.
 state :: State s :> es => (s -> (a, s)) -> Eff es a
 state f = send (State f)
 
@@ -74,22 +83,27 @@ handleState r = \case
   Put s   -> unsafeIO (writeIORef r s)
   State f -> unsafeIO (atomicModifyIORefCAS r (swap . f))
 
+-- | Run the 'State' effect with an initial value for the mutable state.
 runState :: s -> Eff (State s : es) a -> Eff es (a, s)
 runState s m = unsafeState s \r -> do
   x <- interpret (handleState r) m
   s' <- unsafeIO (readIORef r)
   pure (x, s')
 
+-- | Allows you to throw error values of type @e@ and catching these errors too.
 data Error (e :: Type) :: Effect where
   Throw :: e -> Error e m a
   Catch :: m a -> (e -> m a) -> Error e m a
 
+-- | Throw an error.
 throw :: Error e :> es => e -> Eff es a
 throw e = send (Throw e)
 
+-- | Catch any error thrown by a computation and handle it with a function.
 catch :: Error e :> es => Eff es a -> (e -> Eff es a) -> Eff es a
 catch m h = send (Catch m h)
 
+-- | Catch any error thrown by a computation and return the result as an 'Either'.
 try :: Error e :> es => Eff es a -> Eff es (Either e a)
 try m = catch (Right <$> m) (pure . Left)
 
@@ -98,16 +112,20 @@ handleError = \case
   Throw e   -> abort (pure $ Left e)
   Catch m f -> either f pure =<< interpose (handleError @e) (Right <$> m)
 
+-- | Run the 'Error' effect. If there is any unhandled error, it is returned as a 'Left'.
 runError :: ∀ e es a. Eff (Error e : es) a -> Eff es (Either e a)
 runError = interpret (handleError @e) . fmap Right
 
+-- | Provides an append-only state, and also allows you to record what is appended in a specific scope.
 data Writer (w :: Type) :: Effect where
   Tell :: w -> Writer w m ()
   Listen :: m a -> Writer w m (a, w)
 
+-- | Append a value to the state.
 tell :: Writer w :> es => w -> Eff es ()
 tell x = send (Tell x)
 
+-- | Record what is appended in a specific scope.
 listen :: Writer w :> es => Eff es a -> Eff es (a, w)
 listen m = send (Listen m)
 
@@ -120,6 +138,7 @@ handleWriter rs = \case
     pure (x, w')
 {-# INLINABLE handleWriter #-}
 
+-- | Run the 'Writer' state, with the append-only state as a monoidal value.
 runWriter :: Monoid w => Eff (Writer w : es) a -> Eff es (a, w)
 runWriter m = unsafeState mempty \r -> do
   x <- interpret (handleWriter [r]) m
@@ -127,10 +146,12 @@ runWriter m = unsafeState mempty \r -> do
   pure (x, w')
 {-# INLINABLE runWriter #-}
 
+-- | Provides nondeterministic choice.
 data NonDet :: Effect where
   Empty :: NonDet m a
   Choice :: [a] -> NonDet m a
 
+-- | Nondeterministic choice.
 choice :: NonDet :> es => [a] -> Eff es a
 choice etc = send (Choice etc)
 
@@ -145,6 +166,7 @@ handleNonDet = \case
     in collect etc empty
 {-# INLINABLE handleNonDet #-}
 
+-- | Run the 'NonDet' effect, with the nondeterministic choice provided by an 'Alternative' instance.
 runNonDet :: Alternative f => Eff (NonDet : es) a -> Eff es (f a)
 runNonDet = interpret handleNonDet . fmap pure
 {-# INLINABLE runNonDet #-}
